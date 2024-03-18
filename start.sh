0b40913837fa6fef44274b9648c7b7a085bc5ef6
raj141414 <148778305+raj141414@users.noreply.github.com>
2024-03-18 20:44:30 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
-cd /TheMovieProviderBot
+cd /DQ-The-File-Donor
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
