1990597394a3c57967e5c29aa276e81d16cb5089
raj141414 <148778305+raj141414@users.noreply.github.com>
2024-03-18 20:26:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/raj141414/TheMovieProviderBot /TheMovieProviderBot
+  git clone https://github.com/raj141414/TheMovieProviderBot /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TheMovieProviderBot
+  git clone $UPSTREAM_REPO /DQ-The-File-Donor
 fi
 cd /TheMovieProviderBot
 pip3 install -U -r requirements.txt
-echo "Starting TheMovieProviderBot...."
+echo "Starting DQ-The-File-Donor...."
 python3 bot.py
