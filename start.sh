23afaffe1d1884f283413bbfe72f35842f241578
raj141414 <148778305+raj141414@users.noreply.github.com>
2024-03-18 20:46:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/raj141414/TheMovieProviderBot /DQ-The-File-Donor
+  git clone https://github.com/raj141414/TheMovieProviderBot.git /DQ-The-File-Donor
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-The-File-Donor
